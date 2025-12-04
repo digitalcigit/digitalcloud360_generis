3bb799242e8ff1d37880a0702a8268cbb1399166
agnissaneric <agnissan@digital.ci>
2025-12-04 19:39:50 +0000

    fix(logger): Export logger instance to prevent ImportError

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -3,5 +3,6 @@ import structlog
 
 def setup_logging():
     logging.basicConfig(level=logging.INFO)
-    logger = structlog.get_logger()
-    return logger
+    return structlog.get_logger()
+
+logger = structlog.get_logger()
